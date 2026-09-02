b63c1bc2360f84b60a4dc34e6d6dc8d1ab8526f3
Rushi <arushwanth@stratapps.com>
2026-09-02 10:11:41 +0530

    integration commit

--- smartops/integrations/admin.py
+++ smartops/integrations/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import webhookendpoint, webhookdelivery
 
-# Register your models here.
+@admin.register(webhookendpoint)
+class WebhookEndpointAdmin(admin.ModelAdmin):
+    list_display = ("name", "url", "is_active", "created_by", "created_at", "updated_at")
+    search_fields = ("name", "url")
+    list_filter = ("is_active", "created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+    
+@admin.register(webhookdelivery)
+class WebhookDeliveryAdmin(admin.ModelAdmin):
+    list_display = ("endpoint", "event_type", "status", "response_status", "delivered_at", "attempt_count", "created_at")
+    search_fields = ("endpoint__name", "event_type")
+    list_filter = ("status", "delivered_at", "created_at")
+    readonly_fields = ("created_at", "delivered_at")
+    
+
+
